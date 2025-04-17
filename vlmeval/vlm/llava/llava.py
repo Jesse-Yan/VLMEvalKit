5ebd090a3aaff1fb916c7b905d6002ed0838cac9
Jesse-Yan <cyan46@wisc.edu>
2025-04-17 04:47:02 -0500

    minor

--- vlmeval/vlm/llava/llava.py
+++ vlmeval/vlm/llava/llava.py
@@ -10,7 +10,7 @@ import copy
 import requests
 
 import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "..")))
 from LlaVaONNXRunner import VisionLanguageModelONNX
 
 
